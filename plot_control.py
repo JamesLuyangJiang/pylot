a3851c2a0887320537724ec36df15908ae9c1846
James Jiang <luyang.jiang2002@outlook.com>
2023-09-09 15:57:27 -0700

    Change plot file for argument and png directory

--- plot_control.py
+++ plot_control.py
@@ -1,35 +1,57 @@
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+import argparse
+import os
+from pathlib import Path
 
-controlList = []
+def main():
+    controlList = []
+    result_path = ""
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file_path", required=True, help="path of the control json file")
+    args = ap.parse_args()
 
-print("File Reading Starts")
-with open('control.json') as file:
-    for jsonObj in file:
-        controlDict = json.loads(jsonObj)
-        controlDict['timestamp'] = int(controlDict['timestamp'].strip("[]"))
-        controlList.append(controlDict)
-print("File Reading Ends")
+    if args.file_path:
+        if os.path.exists(args.file_path):
+            # Read the file
+            with open(args.file_path) as file:
+                print("file reading starts")
+                # Create the dictionary for each json object and add to a list
+                for jsonObj in file:
+                    controlDict = json.loads(jsonObj)
+                    controlDict['timestamp'] = int(controlDict['timestamp'].strip("[]"))
+                    controlList.append(controlDict)
+            print("file reading ends")
+        else:
+            print("file path does not exist")
 
-df = pd.DataFrame(controlList)
+        df = pd.DataFrame(controlList)
 
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 8))
+        # Config the layout of subplots
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 8))
 
-df.plot(x='timestamp', y='speed', ax=axes[0][0], ylabel='km/h')
-df.plot(x='timestamp', y='steer', ax=axes[0][1], ylabel='rad')
-df.plot(x='timestamp', y='throttle', ax=axes[1][0])
-df.plot(x='timestamp', y='brake', ax=axes[1][1])
+        # Config each subplot
+        df.plot(x='timestamp', y='speed', ax=axes[0][0], ylabel='km/h')
+        df.plot(x='timestamp', y='steer', ax=axes[0][1], ylabel='rad')
+        df.plot(x='timestamp', y='throttle', ax=axes[1][0])
+        df.plot(x='timestamp', y='brake', ax=axes[1][1])
 
-plt.savefig('control_plot.png')
+        # Save the plot back to the path of the control json file
+        result_path = os.path.split(args.file_path)[0]
 
-print("Plot has been saved to control_plot.png")
+        plt.savefig(Path(result_path)/'control_plot.png')
 
-# test1 = controlList[0]
-# test2 = controlList[1]
+        print("plot has been saved to {}\control_plot.png".format(result_path))
+    else:
+        print("please provide valid path to the control json file")
 
-# print(type(test1['timestamp']))
+if __name__ == '__main__':
 
-# print(f"Timestamp: {test1['timestamp']}, Speed: {test1['speed']}, Steer: {test1['steer']}, Throttle: {test1['throttle']}, Break: {test1['brake']}\n")
+    try:
+        main()
 
-# print(f"Timestamp: {test2['timestamp']}, Speed: {test2['speed']}, Steer: {test2['steer']}, Throttle: {test2['throttle']}, Break: {test2['brake']}\n")
+    except KeyboardInterrupt:
+        print('\nPlotting cancelled by user. Bye!')
+    except RuntimeError as e:
+        print(e)
